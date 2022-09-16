1a99ad427ff2676282edaa59b87d240cfb0898fb
Olim Gulomov <113071359+olimgulomov98@users.noreply.github.com>
2022-09-16 10:51:16 +0400

    state, setState, onClick, onChange

--- rfile/src/index.js
+++ rfile/src/index.js
@@ -1,37 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-// import Footer from './Footer'
-// import {Navbar, Products} from './Navbar'
-import Navbar from './Navbar'
-
-const data = [
-  {name: 'React', color: 'red'},
-  {name: 'html', color: 'black'},
-  {name: 'css', color: 'green'},
-  {name: 'javascript', color: 'blue'}
-]
+import './index.css'
+import State from './State';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-      {
-        data.map((value) => {
-          return (
-            <Navbar title={value} />
-          )
-        })
-      }
-      {/* <Navbar title={{name: 'React', color: 'red'}} /> */}
-      {/* <Navbar title='Html' color='black' /> */}
-      {/* <Navbar title='Css' color='blue' bg='green' /> */}
+      <State />
     </React.StrictMode>
-
-  // <React.StrictMode>
-  //   <h1>Hello world</h1>
-  //   <Navbar />
-  //   <Footer />
-  //   <Products />
-  // </React.StrictMode>
 );
 
--- topic.js
+++ topic.js
@@ -1,6 +1,7 @@
-// - class component
-// - componanets
-// - props
-// - children
-// - export, import 
-// - small examples
+// 4-dars
+// - state (ekrandagi malumotni uzgartirib berish uchun kerak boladi)
+// - constructor (ishga tushurib beradi va ichida props ni oladi)
+// - super (obyektga malumotni yuboradi yani componentga)
+// - onChange, onClick
+// - input, checkbox, select
+// - setState (dastlabki qiymatni uzgartirib berish uchun ekranda va render ni bir marta aylantirib beradi)
